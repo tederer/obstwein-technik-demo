4f6c86f2e05ae8679561b9c66151779bd0342538
Thomas Ederer <2210781006@fh-burgenland.at>
2023-06-08 15:16:44 +0200

    initial commit

---
+++ installServices.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -v
+
+set -e
+
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get --assume-yes install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt --assume-yes install docker-compose
+
+sudo apt install net-tools
+sudo echo >> /etc/hosts
+sudo echo "127.0.0.1 provider" >> /etc/hosts
